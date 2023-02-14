3ab4becf9e5d98367ce2f98298e098ababe622eb
Lucasdaniel314 <lucasdanielmonteirodesousa827@gmail.com>
2023-02-14 14:42:41 -0300

    ciando as classes

---
+++ src/model/entidade/HotelCheckin.java
@@ -0,0 +1,62 @@
+package model.entidade;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class HotelCheckin {
+
+	private Integer numeroDoQuarto;
+	private Date checkIn;
+	private Date checkOut;
+	
+	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public HotelCheckin() {
+	}
+
+	public HotelCheckin(Integer numeroDoQuarto, Date checkin, Date checkout) {
+		this.numeroDoQuarto = numeroDoQuarto;
+		this.checkIn = checkin;
+		this.checkOut = checkout;
+	}
+
+	public Integer getNumeroDoQuarto() {
+		return numeroDoQuarto;
+	}
+
+	public void setNumeroDoQuarto(Integer numeroDoQuarto) {
+		this.numeroDoQuarto = numeroDoQuarto;
+	}
+
+	public Date getCheckin() {
+		return checkIn;
+	}
+
+	public Date getCheckout() {
+		return checkOut;
+	}
+	
+	public long duracao() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void atualizarDatas(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Reserva: quarto "
+				+ numeroDoQuarto
+				+ ", checkin: "
+				+ sdf.format(checkIn)
+				+ ", checkout: "
+				+ sdf.format(checkOut)
+				+ ", duracao: "
+				+ duracao()
+				+ " noites.";
+	}
+}
